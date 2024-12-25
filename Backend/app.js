ecb07965fb4614dc0c5c24ed3949dd592247b3ef
Premkumar <premkumar.ashokan@talentship.io>
2024-12-25 13:44:20 +0530

    feature/fetching_data

--- Backend/app.js
+++ Backend/app.js
@@ -1,56 +1,70 @@
-import fs from 'node:fs/promises';
+import fs from "node:fs/promises";
 
-import bodyParser from 'body-parser';
-import express from 'express';
-
+import bodyParser from "body-parser";
+import express from "express";
+import path from "path";
 const app = express();
+const __dirname = path.resolve();
 
-app.use(express.static('backend/images'));
+app.use(express.static("backend/images"));
 app.use(bodyParser.json());
 
 // CORS
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   next();
 });
-
-app.get('/places', async (req, res) => {
-  const fileContent = await fs.readFile('./backend/data/places.json');
-
-  const placesData = JSON.parse(fileContent);
-
-  res.status(200).json({ places: placesData });
+app.get("/", async (req, res) => {
+  res.status(200).json({ success: "hello worl" });
 });
 
-app.get('/user-places', async (req, res) => {
-  const fileContent = await fs.readFile('./backend/data/user-places.json');
+console.log("dir:", __dirname);
+
+app.get("/places", async (req, res) => {
+  try {
+    // Use __dirname to get the absolute path to the current file
+    console.log(__dirname);
+
+    const filePath = path.join(__dirname, "data", "places.json");
+    const fileContent = await fs.readFile(filePath, "utf8");
+    const placesData = JSON.parse(fileContent);
+
+    res.status(200).json({ places: placesData });
+  } catch (err) {
+    console.error("Error reading file:", err.message);
+    res.status(500).json({ error: "Could not retrieve places data" });
+  }
+});
+
+app.get("/user-places", async (req, res) => {
+  const filePath = path.join(__dirname, "data", "user-places.json");
+  const fileContent = await fs.readFile(filePath);
 
   const places = JSON.parse(fileContent);
 
   res.status(200).json({ places });
 });
 
-app.put('/user-places', async (req, res) => {
+app.put("/user-places", async (req, res) => {
   const places = req.body.places;
+  const filePath = path.join(__dirname, "data", "user-places.json");
+  await fs.writeFile(filePath, JSON.stringify(places));
 
-  await fs.writeFile('./backend/data/user-places.json', JSON.stringify(places));
-
-  res.status(200).json({ message: 'User places updated!' });
+  res.status(200).json({ message: "User places updated!" });
 });
 
 // 404
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
-  res.status(404).json({ message: '404 - Not Found' });
+  res.status(404).json({ message: "404 - Not Found" });
 });
 
-app.listen(3000,()=>{
-    console.log("server running 3000");
-    
+app.listen(3000, () => {
+  console.log("server running 3000");
 });
--- Handling_sideEffects/src/components/AvailablePlaces.jsx
+++ Handling_sideEffects/src/components/AvailablePlaces.jsx
@@ -1,12 +1,23 @@
 import Places from "./Places"
-
+import { useEffect, useState } from "react"
 const AvailablePlaces = ({onSelectPlace}) => {
 
 // here we want to write the fetching data API request
+const [availablePlaces, setAailablePlaces] = useState([])
+
+useEffect(()=>{
+    fetch("http://localhost:3000/places").then((response)=>{
+       return response.json()
+    }).then((rData)=>{
+       setAailablePlaces(rData.places)
+    })
+},[])
+
 
   return (
    <Places
      title="Available Places"
+     places={availablePlaces}
      fallbackText="No places available."
    onSelectPlace={onSelectPlace}
    />
